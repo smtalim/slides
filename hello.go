3c2200d44f402de11b1a033e62f5778334d73b32
Satish Talim <satish@rubylearning.org>
2014-03-10 13:49:25 +0530

    commit

--- hello.go
+++ hello.go
@@ -3,5 +3,5 @@ package main  // this file is part of package "main"
 import "fmt"  // this file uses package "fmt"
 
 func main() {
-	fmt.Println("Hello, world.")  // fmt is imported pkg's name
+        fmt.Println("Hello, world.")  // fmt is imported pkg's name
 }
